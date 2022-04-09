0d100ad03e68392404bd8260a7fc54771f759652
pooria <pooria.maleki89@yahoo.com>
2022-04-09 11:51:56 +0430

    create profile_page.dart , order_page.dart , favorite_page.dart .

--- lib/src/screens/main_screen.dart
+++ lib/src/screens/main_screen.dart
@@ -1,27 +1,33 @@
-
 import 'package:flutter/material.dart';
-
+import 'package:food/src/pages/favorite_page.dart';
 // pages
 import 'package:food/src/pages/home_page.dart';
+import 'package:food/src/pages/order_page.dart';
+import 'package:food/src/pages/profile_page.dart';
 
 class MainScreen extends StatefulWidget {
-
   @override
   _MainScreenState createState() => _MainScreenState();
 }
 
 class _MainScreenState extends State<MainScreen> {
-
   int currentTabIndex = 0;
-  late List<Widget>pages;
-  late HomePage homepage;
+  late List<Widget> pages;
   late Widget currentPage;
+
+  late HomePage homepage;
+  late OrderPage orderPage;
+  late FavoritePage favoritePage;
+  late ProfilePage profilePage;
 
   @override
   void initState() {
     super.initState();
     homepage = HomePage();
-    pages = [homepage];
+    orderPage = OrderPage();
+    favoritePage = FavoritePage();
+    profilePage = ProfilePage();
+    pages = [homepage, orderPage, favoritePage, profilePage];
     currentPage = homepage;
   }
 
@@ -38,22 +44,12 @@ class _MainScreenState extends State<MainScreen> {
         currentIndex: currentTabIndex,
         type: BottomNavigationBarType.fixed,
         items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-              label: 'Home'
-          ),
+              icon: Icon(Icons.shopping_cart), label: 'Order'),
           BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            label: 'Order'
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-              label: 'Favorites'
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-              label: 'Profile'
-          ),
+              icon: Icon(Icons.favorite), label: 'Favorites'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
         ],
       ),
       body: currentPage,
