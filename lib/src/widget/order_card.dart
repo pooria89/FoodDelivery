a8b83ebd3bac8df9a915715b48e609516344104c
pooria <pooria.maleki89@yahoo.com>
2022-04-09 18:21:21 +0430

    add order_page.dart uncomplete.

--- lib/src/widget/order_card.dart
+++ lib/src/widget/order_card.dart
@@ -5,52 +5,130 @@ class OrderCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(10.0),
-      child: Card(
-        child: Row(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Container(
-                width: 45.0,
-                height: 75.0,
-                decoration: BoxDecoration(
-                    border: Border.all(width: 1.0, color: Colors.grey),
-                    borderRadius: BorderRadius.circular(10.0)),
-                child: Column(
-                  children: [
-                    InkWell(
-                      child: Icon(Icons.keyboard_arrow_up),
+    return Column(
+      children: [
+        SizedBox(
+          height: 50.0,
+        ),
+        Text(
+          'Your Food Cart',
+          style: TextStyle(fontSize: 25.0, color: Colors.black),
+        ),
+        Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Card(
+            child: Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Container(
+                    width: 45.0,
+                    height: 75.0,
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 1.0, color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Column(
+                      children: [
+                        InkWell(
+                          child: Icon(Icons.keyboard_arrow_up),
+                        ),
+                        Text('0'),
+                        InkWell(
+                          child: Icon(Icons.keyboard_arrow_down),
+                        ),
+                      ],
                     ),
-                    Text('0'),
-                    InkWell(
-                      child: Icon(Icons.keyboard_arrow_down),
+                  ),
+                ),
+                SizedBox(
+                  width: 5.0,
+                ),
+                Container(
+                  width: 60.0,
+                  height: 60.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('asset/images/sandwich.jpeg'),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(35.0),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 10.0,
+                          offset: Offset(0.0, 5.0)),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Pizza',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '3.0',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.yellow.shade600,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'Chicken',
+                          style: TextStyle(fontSize: 12.0, color: Colors.grey),
+                        ),
+                        SizedBox(
+                          width: 3.0,
+                        ),
+                        Icon(
+                          Icons.close,
+                          size: 20.0,
+                          color: Colors.red,
+                        ),
+                        SizedBox(
+                          width: 3.0,
+                        ),
+                        Text(
+                          'Meat',
+                          style: TextStyle(fontSize: 12.0, color: Colors.grey),
+                        ),
+                        SizedBox(
+                          width: 3.0,
+                        ),
+                        Icon(
+                          Icons.close,
+                          size: 20.0,
+                          color: Colors.red,
+                        )
+                      ],
                     ),
                   ],
                 ),
-              ),
+              ],
             ),
-            SizedBox(
-              width: 5.0,
-            ),
-            Container(
-              width: 60.0,
-              height: 60.0,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('asset/images/sandwich.jpeg'),
-                  fit: BoxFit.cover,
-                ),
-                borderRadius: BorderRadius.circular(35.0),
-                boxShadow: [
-                  BoxShadow(color: Colors.black12, blurRadius: 10.0,offset: Offset(0.0,5.0)),
-                ],
-              ),
-            )
-          ],
+          ),
         ),
-      ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.0),
+          child: Row(
+            children: [
+              Text(
+                'Cart Total',
+                style: TextStyle(fontSize: 20.0, color: Colors.grey),
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
